3914a8aebcdb66cd2e75c2ae4bdad0a4f115d7d1
Luis <luiss18.code@gmail.com>
2025-01-28 17:56:46 -0300

    correçao

--- src/app/index/styles.ts
+++ src/app/index/styles.ts
@@ -65,10 +65,11 @@ export const styles = StyleSheet.create({
         color: colors.gray[300],
         fontSize: 14,
         marginTop: 20,
+        paddingBottom: 5
     },
     modalDate: {
         color: colors.gray[400],
-        fontSize: 14,
+        fontSize: 8,
         marginBottom: 20,
         borderBottomColor: colors.gray[600],
         borderBottomWidth: 2,
